5609efce55c9db5a64b9c9e5110e15093593a557
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-05 23:24:42 +0530

    Imported necessary libraries

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,27 @@ import 'package:radiant_residencies_app/contact_us.dart';
 import 'package:radiant_residencies_app/services.dart';
 import 'package:radiant_residencies_app/welcome_page.dart';
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-        options: FirebaseOptions(
-            apiKey: "AIzaSyAuiDWAhkT25gKOBORJm-diQ2NKW3YdXwc",
-            appId: "1:702000616537:web:0a2184a802ed3e7a134d48",
-            messagingSenderId: "702000616537",
-            projectId: "radiant-residencies-330a2"));
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAuiDWAhkT25gKOBORJm-diQ2NKW3YdXwc",
+        appId: "1:702000616537:web:0a2184a802ed3e7a134d48",
+        messagingSenderId: "702000616537",
+        projectId: "radiant-residencies-330a2",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
   }
 
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
